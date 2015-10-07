3babd1e9884abb68556ad222f4bf86e4f40103f0
Jukka Jylänki <jujjyl@gmail.com>
2015-10-07 02:38:48 -0700

    Add -msse2 to build

--- make.py
+++ make.py
@@ -32,7 +32,7 @@ import tools.shared as emscripten
 '''
 emcc_args = sys.argv[1:] or '-O3 --llvm-lto 1 -s NO_EXIT_RUNTIME=1 -s AGGRESSIVE_VARIABLE_ELIMINATION=1 -s NO_DYNAMIC_EXECUTION=1 --memory-init-file 0 -s NO_FILESYSTEM=1 -s NO_BROWSER=1'.split(' ')
 
-emcc_args += ['-s', 'TOTAL_MEMORY=%d' % (64*1024*1024)] # default 64MB. Compile with ALLOW_MEMORY_GROWTH if you want a growable heap (slower though).
+emcc_args += ['-s', 'TOTAL_MEMORY=%d' % (64*1024*1024), '-msse2'] # default 64MB. Compile with ALLOW_MEMORY_GROWTH if you want a growable heap (slower though).
 #emcc_args += ['-s', 'ALLOW_MEMORY_GROWTH=1'] # resizable heap, with some amount of slowness
 
 print
@@ -87,7 +87,7 @@ try:
 
   stage('Build bindings')
 
-  args = ['-I../src', '-c']
+  args = ['-I../src', '-msse2', '-c']
   for include in INCLUDES:
     args += ['-include', include]
   emscripten.Building.emcc('glue.cpp', args, 'glue.bc')
